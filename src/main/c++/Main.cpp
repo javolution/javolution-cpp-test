9098bb280ca00d4a0df7efa979443f52b2884cc3
Jean-Marie Dautelle <jean-marie@dautelle.com>
2017-02-17 23:33:36 +0100

    Multi-thread example.

---
+++ src/main/c++/Main.cpp
@@ -0,0 +1,53 @@
+/*
+ * Javolution - Java(TM) Solution for Real-Time and Embedded Systems
+ * Copyright (C) 2012 - Javolution (http://javolution.org/)
+ * All rights reserved.
+ */
+
+#include "java/lang/Thread.hpp"
+#include "java/lang/System.hpp"
+#include "java/lang/StringBuilder.hpp"
+#include "java/lang/Double.hpp"
+#include "java/lang/Class.hpp"
+#include "java/lang/SecurityException.hpp"
+
+
+/* Unit test main routine */
+int main(int, char**) {
+
+    // By extending Thread class
+    class MyThread : public virtual Thread {
+    public:
+        MyThread(Value* value) : Object(value) {}
+        class Value : public Thread::Value {
+            void run() override {
+                System::out.println("Thread say Hello");
+                Thread::sleep(1000);
+                System::out.println("Thread say Bye!");
+            }
+        };
+    };
+
+    // By implementing Runnable interface.
+    class MyRunnable : public virtual Runnable {
+    public:
+        MyRunnable(Value* value) : Object(value) {}
+        class Value : public Object::Value, public virtual Runnable::Interface {
+            void run() override {
+                System::out.println("Runnable say Hello");
+                Thread::sleep(500);
+                System::out.println("Runnable say Bye!");
+            }
+        };
+    };
+
+
+    MyThread threadA = new MyThread::Value();
+    MyRunnable myRunnable = new MyRunnable::Value();
+    Thread threadB = Thread::newInstance(myRunnable);
+
+    threadA.start();
+    threadB.start();
+    System::out.println("Wait");
+    Thread::sleep(2000);
+}
