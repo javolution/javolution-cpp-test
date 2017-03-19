fee446f4a39ba341725472cf421cd1345b07c694
Jean-Marie Dautelle <owner@javolution.org>
2017-03-19 18:10:26 +0100

    Added parameter passing performance test (by value and by const&)

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # javolution-cpp-test
 Test Suite (JUnit like) for Javolution C++ 
 
-Value-types (stack allocated) show 30x faster allocation/deallocation (Visual C++ 2017)
+Value-types (stack allocated) show 30x faster allocation/deallocation speed (Visual C++ 2017)
 
 ```cpp
- -- JAVOLUTION C++ TESTING --
+  -- JAVOLUTION C++ TESTING --
 java::lang::ObjectTest::testStandardHeap
-Number of objects allocated and destroyed per second: 1.20627e+07
+Number of objects allocated and destroyed per second: 1.36426e+07
 java::lang::ObjectTest::testFastHeap
-Number of objects allocated and destroyed per second: 2.40385e+07
+Number of objects allocated and destroyed per second: 2.48139e+07
 java::lang::ObjectTest::testStack
-Number of objects allocated and destroyed per second: 7.14286e+08
+Number of objects allocated and destroyed per second: 9.09091e+08
+java::lang::ObjectTest::testValuePassing
+Cost of passing smart pointers by value: 0.0168 μs
+java::lang::ObjectTest::testConstRefPassing
+Cost of passing smart pointers by const reference (const&): 0.0003 μs
 java::lang::StringTest::testValueOf_uchar
 java::lang::ThreadTest::testExtendingThread
 java::lang::ThreadTest::testRunnableThread
-Number of test cases run: 6
+Number of test cases run: 8
 Number of error(s) : 0
 Number of failure(s) : 0
 SUCCESS !
--- src/main/c++/java/lang/ObjectTest.hpp
+++ src/main/c++/java/lang/ObjectTest.hpp
@@ -21,6 +21,12 @@ public:
     public:
         static const int COUNT = 10000000;
         static const int USAGE = 1000000;
+        static bool isNullByValue(Object obj) {
+            return obj == nullptr;
+        }
+        static bool isNullByConstRef(const Object& obj) {
+            return obj == nullptr;
+        }
 
         void testStandardHeap() {
             static Object vector[USAGE];
@@ -48,8 +54,8 @@ public:
                             + String::valueOf(COUNT * 1000.0 / (end - start)));
             FastHeap::disable();
             Type::int64 systemHeapCount = FastHeap::getSystemHeapCount();
-            assertTrue("Number of system heap allocations should be zero but was: " +
-                    String::valueOf(systemHeapCount), systemHeapCount == 0);
+            assertTrue("Number of system heap allocations should be zero but was: " + String::valueOf(systemHeapCount),
+                    systemHeapCount == 0);
         }
 
         void testStack() {
@@ -64,21 +70,71 @@ public:
                             + String::valueOf(COUNT * 1000.0 / (end - start)));
         }
 
+        void testValuePassing() {
+            static Object vector[USAGE];
+            for (int i = 0; i < USAGE; i++) {
+                vector[i] = new Object::Value();
+            }
+
+            Type::int64 start = System::currentTimeMillis();
+            for (int i = 0; i < COUNT; i++)
+                if (vector[i % USAGE] == nullptr)
+                    fail();
+            Type::int64 end = System::currentTimeMillis();
+            Type::int64 offset = end - start;
+
+            start = System::currentTimeMillis();
+            for (int i = 0; i < COUNT; i++)
+                if (isNullByValue(vector[i % USAGE]))
+                    fail();
+            end = System::currentTimeMillis();
+            Type::int64 time = end - start - offset;
+
+            System::out.println("Cost of passing smart pointers by value: "
+                    + String::valueOf(time * 1000.0 / COUNT) + u" μs");
+        }
+
+        void testConstRefPassing() {
+              static Object vector[USAGE];
+              for (int i = 0; i < USAGE; i++) {
+                  vector[i] = new Object::Value();
+              }
+
+              Type::int64 start = System::currentTimeMillis();
+              for (int i = 0; i < COUNT; i++)
+                  if (vector[i % USAGE] == nullptr)
+                      fail();
+              Type::int64 end = System::currentTimeMillis();
+              Type::int64 offset = end - start;
+
+              start = System::currentTimeMillis();
+              for (int i = 0; i < COUNT; i++)
+                  if (isNullByConstRef(vector[i % USAGE]))
+                      fail();
+              end = System::currentTimeMillis();
+              Type::int64 time = end - start - offset;
+
+              System::out.println("Cost of passing smart pointers by const reference (const&): "
+                      + String::valueOf(time * 1000.0 / COUNT) + u" μs");
+          }
     };
 
     CLASS_BASE (ObjectTest, TestCase)
     TEST (testStandardHeap)
     TEST (testFastHeap)
     TEST (testStack)
+    TEST (testValuePassing)
+    TEST (testConstRefPassing)
 
     static TestSuite suite() {
         TestSuite tests = new TestSuite::Value();
         tests.addTest(new testStandardHeap());
         tests.addTest(new testFastHeap());
         tests.addTest(new testStack());
+        tests.addTest(new testValuePassing());
+        tests.addTest(new testConstRefPassing());
         return tests;
     }
 };
 
-}
-}
+}}
